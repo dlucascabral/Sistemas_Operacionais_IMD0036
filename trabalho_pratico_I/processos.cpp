81e9cd4fe64248f2537f5bf2d56a084deb99a964
EdvaldoUFRN <edvaldo_junior2009@hotmail.com>
2023-04-19 16:01:15 -0300

    adição da função que calcula o tempo em processos.cpp

--- trabalho_pratico_I/processos.cpp
+++ trabalho_pratico_I/processos.cpp
@@ -1,8 +1,12 @@
+#include <iostream>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <chrono>
+
+using namespace std;
 
 int main(int argc, char *argv[]){
     
@@ -12,19 +16,23 @@ int main(int argc, char *argv[]){
     int m2 = atoi(argv[4]);    
     pid_t pid;
     pid = fork();
+   chrono::steady_clock::time_point begin = chrono::steady_clock::now();
     if (pid<0)
     {
         printf("Erro ao criar processo \n");
         return 1;
     }
+    
     else if (pid==0)
     {
         printf("Esta é a execução do filho(PID=%d), cujo pai tem PID=%d\n", getpid(),getppid());
         sleep(3);
-    }
+    } 
     else{
         wait(NULL);
         printf("Processo-Filho finalizou\n");
     }
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout<<"Tempo" << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << "[ms]" <<endl;
     return 0;
 }
