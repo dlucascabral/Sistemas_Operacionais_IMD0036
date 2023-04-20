ce2b1457b3fdb7cc789a7586e61d9880166591a0
EdvaldoUFRN <edvaldo_junior2009@hotmail.com>
2023-04-20 00:23:37 -0300

    falta acerta a logica da matriz multiplicação

--- trabalho_pratico_I/processos.cpp
+++ trabalho_pratico_I/processos.cpp
@@ -29,14 +29,90 @@ int main(int argc, char *argv[]){
         matriz1[i] = (int *)calloc(coluna1, sizeof(int));
     }
     
+    matriz2 = (int **)calloc(linha2,sizeof(int*));
+    for (int i = 0; i < linha2; i++)
+    {
+        matriz2[i] = (int *)calloc(coluna2,sizeof(int));
+    }
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    //gerar valores aleatorios
+    for (int i = 0; i < linha1; i++)
+    {
+        for (int j = 0; j < coluna1; j++)
+        {
+            matriz1[i][j] = (rand()%10 +1);
+        }
+        
+    }
+    
+    for (int i = 0; i < linha2; i++)
+    {
+        for (int j = 0; j < coluna2; j++)
+        {
+            matriz2[i][j] = (rand()%10 +1);
+        }
+        
+    }
+
+    //imprimir elas
+    for (int i = 0; i < linha1; i++)
+    {
+        for (int j = 0; j < coluna1; j++)
+        {
+            printf("%5d",matriz1[i][j]);
+        }
+        cout<<endl;
+        
+    }
+    cout<<"                                  "<<endl;
+    for (int i = 0; i < linha2; i++)
+    {
+        for (int j = 0; j < coluna2; j++)
+        {
+            printf("%5d",matriz2[i][j]);
+        }
+        cout<<endl;
+        
+    }
+
+    //matriz resultante
+    
+    matrizresultante = (int **)calloc(linha1, sizeof(int *));
+    for (int i = 0; i < linha1; i++)
+    {
+        matrizresultante[i] = (int *)calloc(coluna2, sizeof(int));
+    }
+    int res;
+    //Calculando
+    for (int i = 0; i < linha1; i++)
+    {
+        for (int j = 0; j < coluna2; j++)
+        {
+            int a = j;
+           matrizresultante[i][j]   = matrizresultante[i][j] + (matriz1[i][j] * matriz2[j][i]);
+             
+        }
+         
+    }
+    cout<<"                                  "<<endl;
+    for (int i = 0; i < linha1; i++)
+    {
+        for (int j = 0; j < coluna2; j++)
+        {
+            printf("%5d",matrizresultante[i][j]);
+        }
+        cout<<endl;
+        
+    }
+    
     //Para iterar o valor do P eu tenho que fazer o calculo
     //das matrizes pra descobrir quantas colunas eu tenho.
     //depois da multiplicação.
     //faz um exit pra finalizar o processo depois de fazer a resultante.
     //assim descendo até todas as linhas.
-
-    pid = fork();
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    /*int pid;
+    pid = fork();*/
+    
     
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
     cout<<"Tempo" << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << "[ms]" <<endl;
