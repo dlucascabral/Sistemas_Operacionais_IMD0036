69a91e0a777420165ef0ace69829d60f64b7e73e
EdvaldoUFRN <edvaldo_junior2009@hotmail.com>
2023-04-21 18:41:13 -0300

    finalizado processos.cpp

--- trabalho_pratico_I/processos.cpp
+++ trabalho_pratico_I/processos.cpp
@@ -3,7 +3,8 @@
 #include <sys/wait.h>
 #include <sys/shm.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <sys/ipc.h>
+#include <cstdlib>
 #include <chrono>
 #include <time.h>
 using namespace std;
@@ -29,25 +30,20 @@ int main(int argc, char *argv[]){
         cout<<"Erro, as dimensões escolhidas não são permitidas para a multiplicação de matrizes."<<endl;
         return 1;
     }
-    
-    /*int seg_id = shmget(IPC_PRIVATE, 20*sizeof(char), IPC_CREAT | 0666);*/
 
  /*alocando dinamicamente as matrizes com a função new*/
     int **matriz1 = new int*[linha1];
     int **matriz2 = new int*[linha2];
-    int **matrizresultante = new int*[linha1];
+    int *matrizresultante;
+//pegando os valores aleatórios para as matrizes 1 e 2 respectivamente.
     for (int i = 0; i < linha1; i++)
     {
         matriz1[i] = new int[coluna1];
-        matrizresultante[i] = new int[coluna2];
     }
     for (int i = 0; i < linha2; i++)
     {
         matriz2[i] = new int[coluna2];
     }
-
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-   /*loop para preencher com numeros aleatórios as matrizes*/
     for (int i = 0; i < linha1; i++)
     {
         for (int j = 0; j < coluna1; j++)
@@ -65,38 +61,80 @@ int main(int argc, char *argv[]){
         }
         
     }
- /*vendo o que eu faço com essa criação de um processo filho.*/   
-  /*  int pid;
-    pid = fork();
-    */
-/*calculo da matriz resultante*/
-    for (int i = 0; i < linha1; i++)
+//criação da memoria partilhada entre os processos.
+    int memoria = shmget(IPC_PRIVATE, linha1 * coluna2 * sizeof(int), IPC_CREAT | 0666);
+    if (memoria == -1)
     {
-        for (int j = 0; j < coluna2; j++)
-        {
-           matrizresultante[i][j] = 0;
-           for (int k = 0; k < coluna1; k++)
-           {
-             matrizresultante[i][j] += matriz1[i][k] * matriz2[k][j];
-           }
-           
-        }
-         
+        cerr << "Erro na criação da memória."<<endl;
+        exit(EXIT_FAILURE);
     }
+    matrizresultante = (int *) shmat(memoria, NULL, 0);
+//criando os processos 1 e 2
 
-    for (int i = 0; i < linha1; i++)
+    pid_t processo1, processo2;
+    int metade = linha1/2;
+    processo1 = fork();
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    if (processo1 < 0)
     {
-        for (int j = 0; j < coluna2; j++)
-        {
-           cout << matrizresultante[i][j]<<"  ";
-        }
-        cout<<endl;
+        cerr<<"Erro na criação do processo1"<<endl;
+        exit(EXIT_FAILURE);
     }
-/*usando a função delete para liberar os espaços utilizados na criação das matrizes*/
+    else if (processo1 == 0)//Aqui o processo 1 vai executar a metade da matriz resultante.
+    {
+        for (int i = 0; i < metade; i++)
+        {
+            for (int j = 0; j < coluna2; j++)
+            {
+                int calculo = 0;
+                for (int k = 0; k < coluna1; k++)
+                {
+                    calculo += matriz1[i][k] * matriz2[k][j];
+                }
+                matrizresultante[(i*coluna2)+j] = calculo;
+            }
+            
+        }
+        shmdt(matrizresultante);//desanexando a memoria partilhada desse processo.
+        exit(EXIT_SUCCESS);
+        
+    }
+    else{ //processo pai
+        processo2 = fork();//O processo 2 vai terminar a outra metade da matriz resultante.
+        if (processo2 < 0)
+        {
+            cerr<<"Erro na criação do processo2."<<endl;
+            exit(EXIT_FAILURE);
+        }
+        else if (processo2 == 0)
+        {
+            for (int i = metade; i < linha1; i++)
+            {
+                for (int j = 0; j < coluna2; j++)
+                {
+                    int calculo = 0;
+                    for (int k = 0; k < coluna1; k++)
+                    {
+                        calculo += matriz1[i][k] * matriz2[k][j];
+                    }
+                    matrizresultante[(i*coluna2)+j] = calculo;
+                }
+                
+            }
+            shmdt(matrizresultante);//desanexando a memoria partilhada desse processo.
+            exit(EXIT_SUCCESS);
+        }
+    }
+//esperando os dois processos acabarem para retomar ao processo pai.
+    for (int i = 0; i < 2; i++)
+    {
+        wait(NULL);
+    }
+    shmctl(memoria, IPC_RMID, NULL);
+//desalocando as memórias.
      for ( int i = 0; i < linha1; i++)
     {
         delete[] matriz1[i];
-        delete[] matrizresultante[i];
     }
 
     for (int i = 0; i < linha2; i++)
@@ -106,12 +144,11 @@ int main(int argc, char *argv[]){
     
     delete[] matriz1;
     delete[] matriz2;
-    delete[] matrizresultante;
  
     
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
     float tempo = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
-    cout<<"Tempo " << tempo/1000 << "[s]" <<endl;
+    cout<<"Tempo " << tempo << "[ms]" <<endl;
     
     return 0;
 }
